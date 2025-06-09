53d2eb851cbe76ca96f701ce9ef4341a3aba1aa3
g-m007 <gauthamhari0@gmail.com>
2025-06-09 19:04:00 +0530

    Modified config Files

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,26 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   build: {
     chunkSizeWarningLimit: 1600,
     rollupOptions: {
-      external: ['@emailjs/browser', 'react-parallax-tilt'],
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          emailjs: ['@emailjs/browser'],
+          tilt: ['react-parallax-tilt']
+        },
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      }
     }
   },
   optimizeDeps: {
